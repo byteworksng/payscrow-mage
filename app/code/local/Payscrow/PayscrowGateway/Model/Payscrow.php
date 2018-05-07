980c0012283f1aa2d46db6131d51d3f93cfc5dc6
Chibuzor Ogbu <chibuzorogbu@gmail.com>
2018-05-07 08:52:58 +0100

    minor update, removed hanging console logs

--- app/code/local/Payscrow/PayscrowGateway/Model/Payscrow.php
+++ app/code/local/Payscrow/PayscrowGateway/Model/Payscrow.php
@@ -18,8 +18,7 @@ class Payscrow_PayscrowGateway_Model_Payscrow extends Mage_Payment_Model_Method_
 
     public function getOrderPlaceRedirectUrl()
     {
-        return Mage::getUrl('payscrowgateway/payment/redirect', array('_secure' => true));
-        //return Mage::getUrl('payscrowgateway/payment/redirect', array('_secure' => isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ));
+        return Mage::getUrl('payscrowgateway/payment/redirect', array('_secure' => isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ));
     }
 
 }
